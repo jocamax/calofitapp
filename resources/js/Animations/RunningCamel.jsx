d5619f0d8e0ebe223c08235fac8a5d1f13090bf1
jocamax <jovan.maksin@gmail.com>
2025-03-04 15:13:31 +0100

    lottie error

--- resources/js/Animations/RunningCamel.jsx
+++ resources/js/Animations/RunningCamel.jsx
@@ -1,27 +1,30 @@
-import { useState } from "react";
-import Lottie from 'react-lottie';
-import animationData from '/public/storage/animations/running_camel.json'
+// import { useState } from "react";
+// import Lottie from 'react-lottie';
+// import animationData from '/public/storage/animations/running_camel.json'
 
 export default function RunningCamel(){
-    const [showAnimation, setShowAnimation] = useState(true);
+    // const [showAnimation, setShowAnimation] = useState(true);
+    //
+    // const defaultOptions = {
+    //     loop: true,
+    //     autoplay: true,
+    //     animationData: animationData,
+    //     renderer: 'svg'
+    // }
 
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animationData,
-        renderer: 'svg'
-    }
+    //THIS DOESNT WORK FOR SOME REASON SO I'LL COME BACK TO IT AFTER DEPLOYMENT
     return (
-        <Lottie
-            options={defaultOptions}
-            height={300}
-            width={300}
-            eventListeners={[
-                {
-                    eventName: "complete",
-                    callback: () => setShowAnimation(false),
-                },
-            ]}
-        />
+        <div>Loading</div>
+        // <Lottie
+        //     options={defaultOptions}
+        //     height={300}
+        //     width={300}
+        //     eventListeners={[
+        //         {
+        //             eventName: "complete",
+        //             callback: () => setShowAnimation(false),
+        //         },
+        //     ]}
+        // />
     )
 }
